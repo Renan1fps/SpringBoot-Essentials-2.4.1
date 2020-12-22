bece985efe5a3b9d8c5dca21025b287063e0d45e
Renan Oliveira <renan.sergipe68@gmail.com>
2020-12-22 19:08:02 -0300

    Spring data JPA pt 01

--- src/main/java/br/com/springessentials2/demo/domain/Anime.java
+++ src/main/java/br/com/springessentials2/demo/domain/Anime.java
@@ -2,10 +2,21 @@ package br.com.springessentials2.demo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data// Vai gerar gets and sets, equals hashcod, tostring;
 @AllArgsConstructor// vai gerar construtores com todos atributos;
+@NoArgsConstructor
+@Entity
 public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     //@JsonProperty("name") para caso do json nao reconhecer o atributo;
