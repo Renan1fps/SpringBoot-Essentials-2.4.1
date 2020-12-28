a0efbf6d63d4f36f9d865a0cdac9a895dd766dc8
Renan Oliveira <renan.sergipe68@gmail.com>
2020-12-28 17:56:12 -0300

    WebMvcConfigurer

---
+++ src/main/java/br/com/springessentials2/demo/configurer/DevdojoWebMvcConfigurer.java
@@ -0,0 +1,19 @@
+package br.com.springessentials2.demo.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class DevdojoWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageable = new PageableHandlerMethodArgumentResolver();
+        pageable.setFallbackPageable(PageRequest.of(0, 5));
+        resolvers.add(pageable);
+    }
+}
