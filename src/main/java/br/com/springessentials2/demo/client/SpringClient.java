c8fa2b509cd5b08ec2034e5331093882ef1160e7
Renan Oliveira <renan.sergipe68@gmail.com>
2020-12-29 18:17:41 -0300

    RestTemplate POST

--- src/main/java/br/com/springessentials2/demo/client/SpringClient.java
+++ src/main/java/br/com/springessentials2/demo/client/SpringClient.java
@@ -3,6 +3,7 @@ package br.com.springessentials2.demo.client;
 import br.com.springessentials2.demo.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -24,5 +25,16 @@ public class SpringClient {
                 new ParameterizedTypeReference<>() {});
         //@formatter:on
         log.info(exchange.getBody());
+
+        Anime kingdom= Anime.builder().name("kingdom").build();
+        Anime animePost = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
+        log.info("Anime saved {}",animePost );
+
+        //@formatter:off
+        Anime cavaleiros= Anime.builder().name("Cavaleiros").build();
+        new RestTemplate().exchange("http://localhost:8080/animes/",HttpMethod.POST,
+                                         new HttpEntity<>(cavaleiros),Anime.class);
+        //@formatter:on
+        log.info("saved anime{}",cavaleiros);
     }
 }
